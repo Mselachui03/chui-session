1b808be9efef87873a172ee5e06ffc8c3e899144
Mselachui03 <148160583+Mselachui03@users.noreply.github.com>
2025-05-19 15:45:26 +0200

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,8 +1,8 @@
 const mega = require("megajs");
 
 const auth = {
-    email: 'lazck22@gmail.com',
-    password: '@Lazack_28',
+    email: 'chuiofficial03@gmail.com',
+    password: 'mselachui03',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
 
