445222666621a520b40f96781a8bc1546cadb9f3
Mselachui03 <148160583+Mselachui03@users.noreply.github.com>
2025-11-15 12:25:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 const express = require('express');
 const app = express();
-__path = process.cwd()
+const __path = process.cwd(); // Added semicolon and fixed variable declaration
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
 let code = require('./pair'); 
 
 require('events').EventEmitter.defaultMaxListeners = 500;
 
-app.use('/code', code);
-app.use('/pair', async (req, res, next) => {
-    res.sendFile(__path + '/pair.html')
-});
-app.use('/', async (req, res, next) => {
-    res.sendFile(__path + '/main.html')
-});
-
+// Middleware should be defined before routes
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Routes
+app.use('/code', code);
+
+app.get('/pair', (req, res) => { // Changed to app.get for better practice
+    res.sendFile(__path + '/pair.html');
+});
+
+app.get('/', (req, res) => { // Changed to app.get for better practice
+    res.sendFile(__path + '/main.html');
+});
 
 app.listen(PORT, () => {
     console.log(`
@@ -24,7 +28,7 @@ Don't Forget To Give Star ‼️
 
 ᴘᴏᴡᴇʀᴅ ʙʏ ʀᴏᴏᴛ_x
 
-Server running on http://localhost:` + PORT)
-});
+Server running on http://localhost:` + PORT);
+}); // Added missing semicolon
 
 module.exports = app;
